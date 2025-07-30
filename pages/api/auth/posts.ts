43c130b657d5366199a359eba42e6ecaa277bb39
Nathnael Andargachew <nathnaelandargachew@gmail.com>
2025-07-30 14:28:52 +0300

    another change

--- pages/api/auth/posts.ts
+++ pages/api/auth/posts.ts
@@ -1,6 +1,6 @@
 // pages/api/posts.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../../lib/prisma";
 import { verifyToken } from "../../../utils/verifyToken";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
